d9a168d403954ab6fc456bc1b9944b0fab4b7bf1
Spencer Lepine <60903378+spencerlepine@users.noreply.github.com>
2021-02-10 09:34:16 -0500

    fix Align Columns

--- Align Columns/align_columns.py
+++ Align Columns/align_columns.py
@@ -2,31 +2,33 @@ def print_columns(text="", justify="left"):
     # Splt the words
     words = text.split('$')
     
-    print(words) 
-    
     # Find the longest word
     longest_word = words[0]
     for word in words:
         if len(word) > len(longest_word):
             longest_word = word
     column_width = len(longest_word)
-   
+
     # Begin making columns
-    for word in range(len(words)):
-        this_word = " " * column_width
+    for word in words:
+        this_word = None #" "  * column_width
             
+        # Make sure \n is at end always
+        newLine = False
+        if '\n' in word:
+            word = word[:-1]
+            newLine = True
+        
         if justify == "right":
-            this_word = words[0].rjust(column_width)
+            this_word = word.rjust(column_width)
         elif justify == "left":
-            this_word = words[0].ljust(column_width)
-        elif justify == "left":
-            this_word = words[0].center(column_width)
+            this_word = word.ljust(column_width)
+        elif justify == "center":
+            this_word = word.center(column_width)
                 
-            print(f"{this_word} ", end="")
-            words.pop(0)
-
- 
-
-
-# Make it so word[0] prints \n within it, if so.
-# Just go through each word and print the corresponing thing
+        print(f"{this_word}", end="")
+        
+        if newLine:
+            print("")
+        else:
+            print(" ", end="")
--- Align Columns/test_align_columns.py
+++ Align Columns/test_align_columns.py
@@ -1,6 +1,6 @@
 from align_columns import print_columns
 
-text = "Given$a$text$file$of$many$lines,$where$fields$within$a$line$are$delineated$by$a$single$'dollar'$character,$write$a$program$that$aligns$each$column$of$fields$by$ensuring$that\$words$in$each$column$are$separated$by$at$least\$one$space."
+text = "Given$a$text$file$of$many$lines,$where$fields$within$a$line\n$are$delineated$by$a$single$'dollar'$character,$write$a$program\n$that$aligns$each$column$of$fields$by$ensuring$that$words$in$each\n$column$are$separated$by$at$least$one$space.\n$Further,$allow$for$each$word$in$a$column$to$be$either$left\n$justified,$right$justified,$or$center$justified$within$its$column.\n"
 
 def Atest_align_center(capsys):
     print_columns(text, "center")
@@ -8,14 +8,15 @@ def Atest_align_center(capsys):
     captured = capsys.readouterr()
     assert captured.out == "something"
     
-def test_align_right(capsys):
+def Atest_align_right(capsys):
     print_columns(text, "right")
     
     captured = capsys.readouterr()
-    assert captured.out == "      Given          a       text       file         of       many     lines,      where     fields     within          a       line\n                   are delineated         by          a     single   'dollar' character,      write          a    program\n       that     aligns       each     column         of     fields         by   ensuring       that      words         in       each\n     column        are  separated         by         at      least        one     space.\n   Further,      allow        for       each       word         in          a     column         to         be     either       left\n justified,      right justified,         or     center  justified     within        its    column.\n"
+    assert captured.out == "     Given          a       text       file         of       many     lines,      where     fields     within          a       line\n       are delineated         by          a     single   'dollar' character,      write          a    program\n      that     aligns       each     column         of     fields         by   ensuring       that      words         in       each\n    column        are  separated         by         at      least        one     space.\n  Further,      allow        for       each       word         in          a     column         to         be     either       left\njustified,      right justified,         or     center  justified     within        its    column.\n"
     
-def Atest_align_left(capsys):
+def test_align_left(capsys):
     print_columns(text, "left")
     
     captured = capsys.readouterr()
-    assert captured.out == "something"
+    
+    assert captured.out == "Given      a          text       file       of         many       lines,     where      fields     within     a          line      \nare        delineated by         a          single     'dollar'   character, write      a          program   \nthat       aligns     each       column     of         fields     by         ensuring   that       words      in         each      \ncolumn     are        separated  by         at         least      one        space.    \nFurther,   allow      for        each       word       in         a          column     to         be         either     left      \njustified, right      justified, or         center     justified  within     its        column.   \n"
