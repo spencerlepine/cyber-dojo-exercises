59b33500aa5fbf60ca430c0091e52e0dfa7d728c
Spencer Lepine <60903378+spencerlepine@users.noreply.github.com>
2021-02-10 09:37:02 -0500

    fix missing test

--- Align Columns/test_align_columns.py
+++ Align Columns/test_align_columns.py
@@ -2,13 +2,13 @@ from align_columns import print_columns
 
 text = "Given$a$text$file$of$many$lines,$where$fields$within$a$line\n$are$delineated$by$a$single$'dollar'$character,$write$a$program\n$that$aligns$each$column$of$fields$by$ensuring$that$words$in$each\n$column$are$separated$by$at$least$one$space.\n$Further,$allow$for$each$word$in$a$column$to$be$either$left\n$justified,$right$justified,$or$center$justified$within$its$column.\n"
 
-def Atest_align_center(capsys):
+def test_align_center(capsys):
     print_columns(text, "center")
     
     captured = capsys.readouterr()
-    assert captured.out == "something"
+    assert captured.out == "  Given        a         text       file        of        many      lines,     where      fields     within       a         line   \n   are     delineated     by         a        single    'dollar'  character,   write        a       program  \n   that      aligns      each      column       of       fields       by      ensuring     that      words        in        each   \n  column      are     separated      by         at       least       one       space.  \n Further,    allow       for        each       word        in         a        column       to         be       either      left   \njustified,   right    justified,     or       center   justified    within      its      column.  \n"
     
-def Atest_align_right(capsys):
+def test_align_right(capsys):
     print_columns(text, "right")
     
     captured = capsys.readouterr()
